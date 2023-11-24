e748b9e3ae6447410759d13cae4a77f71ee63e7f
winter <2436197699@qq.com>
2023-11-24 18:06:33 +0800

    feat: 完善 BeanDefinition 的属性

--- com/minis/beans/BeanDefinition.java
+++ com/minis/beans/BeanDefinition.java
@@ -6,6 +6,8 @@ package com.minis.beans;
  *  </beans>
 */
 public class BeanDefinition {
+    private final String SCOPE_SINGLETON = "singleton";
+    private final String SCOPE_PROTOTYPE = "prototype";
     /**
     * beanName
     */
@@ -18,6 +20,81 @@ public class BeanDefinition {
     public BeanDefinition(String id, String className) {
         this.id = id;
         this.className = className;
+    }
+
+    /**
+    * 是否单例(默认单例)
+    */
+    private String scope = SCOPE_SINGLETON;
+    /**
+    * 懒加载
+    */
+    private boolean lazyInit = false;
+    /**
+    * 当前 bean 依赖的其他 beans(会等待依赖的 beans 初始化完毕)
+    */
+    private String[] dependsOn;
+    /**
+    * 实例化 bean 时需调用的初始化 bean 方法
+    */
+    private String initMethodName;
+    /**
+    * 注入时的参数列表
+    */
+    private ArgumentValues argumentValues;
+    /**
+    * 注入时的属性列表
+    */
+    private PropertyValues propertyValues;
+
+    public BeanDefinition(String id, String className, boolean lazyInit, String[] dependsOn, String initMethodName, ArgumentValues argumentValues, PropertyValues propertyValues) {
+        this.id = id;
+        this.className = className;
+        this.lazyInit = lazyInit;
+        this.dependsOn = dependsOn;
+        this.initMethodName = initMethodName;
+        this.argumentValues = argumentValues;
+        this.propertyValues = propertyValues;
+    }
+
+    public boolean isLazyInit() {
+        return lazyInit;
+    }
+
+    public void setLazyInit(boolean lazyInit) {
+        this.lazyInit = lazyInit;
+    }
+
+    public String[] getDependsOn() {
+        return dependsOn;
+    }
+
+    public void setDependsOn(String[] dependsOn) {
+        this.dependsOn = dependsOn;
+    }
+
+    public String getInitMethodName() {
+        return initMethodName;
+    }
+
+    public void setInitMethodName(String initMethodName) {
+        this.initMethodName = initMethodName;
+    }
+
+    public ArgumentValues getArgumentValues() {
+        return argumentValues;
+    }
+
+    public void setArgumentValues(ArgumentValues argumentValues) {
+        this.argumentValues = argumentValues;
+    }
+
+    public PropertyValues getPropertyValues() {
+        return propertyValues;
+    }
+
+    public void setPropertyValues(PropertyValues propertyValues) {
+        this.propertyValues = propertyValues;
     }
 
     public String getId() {
